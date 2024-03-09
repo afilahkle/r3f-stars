23bd370fbbcee0d4b553979a630af9f410978b42
Omar Khalifa <elkhalifa002@gmail.com>
2024-03-09 10:48:54 +0200

    init r3f scene

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Canvas } from '@react-three/fiber';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('app')).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <Canvas camera={{ position: [0, 0, 2] }}>
+      <mesh>
+        <boxGeometry />
+        <meshBasicMaterial color={0xff0000} />
+      </mesh>
+    </Canvas>
+  </React.StrictMode>
 );
