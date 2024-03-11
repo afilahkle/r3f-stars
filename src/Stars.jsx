28f72b6128fce7f584724f9d6b17015b9fc41abe
Omar Khalifa <elkhalifa002@gmail.com>
2024-03-11 09:27:29 +0200

    add Stars to the scene

---
+++ src/Stars.jsx
@@ -0,0 +1,38 @@
+import { useState, useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { Points, PointMaterial } from '@react-three/drei';
+import * as random from 'maath/random/dist/maath-random.esm';
+
+const Stars = () => {
+  const ref = useRef();
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5100), { radius: 1.2 })
+  );
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
+        ref={ref}
+        positions={sphere}
+        scale={[2, 2, 2]}
+        stride={3}
+        frustumCulled
+      >
+        <PointMaterial
+          transparent
+          color='#f272c8'
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
+
+export default Stars;
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Canvas } from '@react-three/fiber';
+import Stars from './Stars';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
     <Canvas camera={{ position: [0, 0, 2] }}>
-      <mesh>
-        <boxGeometry />
-        <meshBasicMaterial color={0xff0000} />
-      </mesh>
+      <Stars />
     </Canvas>
   </React.StrictMode>
 );
